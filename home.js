bb00349107c3fac1d6b200ef8fa64394e789df47
MbachoJoyce <100560728+MbachoJoyce@users.noreply.github.com>
2024-04-20 01:45:09 +0300

    Add files via upload

---
+++ home.js
@@ -0,0 +1,9 @@
+function toggleContacts() {
+	// body...
+	var content = document.getElementById("contacts");
+	if (content.style.display==="none") {
+		content.style.display="block";
+	}else{
+		content.style.display = "none";
+	}
+}
